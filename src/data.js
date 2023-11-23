26649ff29e37d3854533abdfde7c3cfcfca9a9f5
plamen penchev <ppenchev73@gmail.com>
2023-11-23 13:12:30 +0200

    adjusted fonts

--- src/data.js
+++ src/data.js
@@ -5,7 +5,6 @@
     {
       id: 1,
       title: "Notes",
-      logo: "./assets/linkedin.png",
       description: "The backend serves as the robust foundation for a streamlined note-taking application. ",
       img:
         "./assets/notes.png",
@@ -15,7 +14,6 @@
     {
       id: 2,
       title: "Blog",
-      logo: "./assets/linkedin.png",
       description: "An application designed to handle all CRUD operations and  manages user profiles and blog posts",
       img:
       "./assets/blog.png",
@@ -25,7 +23,6 @@
     {
       id: 3,
       title: "Full stack task tracker",
-      logo: "./assets/linkedin.png",
       description: "An app with a very basic interface that handles registration and crud operations for adding and assigning tasks.",
       img:
       "./assets/blog.png",
@@ -39,7 +36,6 @@
     {
       id: 4,
       title: "Portfolio",
-      logo: "./assets/linkedin.png",
       description: "Welcome to my portfolio website! Here, I proudly present a curated collection of my work, highlighting my skills, creativity, and passion for web development",
       img:
         "./assets/portfolio.png",
@@ -50,23 +46,19 @@
     {
       id: 5,
       title: "Pizza and cocktails",
-      logo: "./assets/linkedin.png",
-      description: "This is an example",
+      description: "React app with context api and styled components for ordering cocktails from an external api",
       img:
         "./assets/pizza.png",
       githubLink: "https://github.com/",
-      liveLink: "https://www.facebook.com/",
       technologies: ["css","react", "javascript"],
     },
     {
       id: 6,
       title: "Markdown notetaking app",
-      logo: "./assets/linkedin.png",
-      description: "This is an example",
+      description: "A markdown editor with a theme switcher, local storage and download and preview functions",
       img:
         "./assets/mdnotes.png",
       githubLink: "https://github.com/",
-      liveLink: "https://www.facebook.com/",
       technologies: ["css","react", "javascript"],
     },
    
@@ -77,7 +69,6 @@
     {
       id: 7,
       title: "Festival Page",
-      logo: "./assets/linkedin.png",
       description: "A page to represent a festival in the city in Berlin, where i developed the theme and some of the plugins.",
       img:
         "https://osteuropa-tage.de/wp-content/uploads/2020/09/Cover_date-1.jpg",
@@ -87,7 +78,6 @@
     {
       id: 8,
       title: "Staedtepartner Stettin",
-      logo: "./assets/linkedin.png",
       description: "Designed and developed the website, coded most of the components from scratch and implemented with elementor.",
       img: "./assets/stettin.png",
       liveLink: "https://staedtepartner-stettin.org/",
@@ -96,7 +86,6 @@
     {
       id: 9,
       title: "Natuerlich Kultur",
-      logo: "./assets/linkedin.png",
       description: "Implemented a shop with a billing system and redesigned most of the components of the existing website.",
       img:
         "./assets/nkultur.png",
